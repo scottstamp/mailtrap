98420f46c4d0cf7af4817f986f2e18a21dbb2eb3
Scott Stamp <scott@hypermine.com>
2026-02-01 00:06:31 -0330

    enable CORS headers for code api

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,35 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  headers: async () => {
+    return [
+      {
+        source: '/api/codes/short',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, max-age=0',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
